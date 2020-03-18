d995e2fec1e33754adbcd4498a67886e585f51fb
acwikla <acwikla@novelaneuro.com>
2020-03-18 17:10:33 +0100

    travis fix

--- conda_upload.sh
+++ conda_upload.sh
@@ -4,7 +4,7 @@
 PKG_NAME=test_release
 USER=acwikla-novela
 
-OS=linux-64
+OS=$TRAVIS_OS_NAME-64
 
 mkdir ~/conda-bld
 conda config --set anaconda_upload no
@@ -12,10 +12,13 @@ export CONDA_BLD_PATH=~/conda-bld
 export VERSION=`date +%Y.%m.%d`
 conda build .
 #ToDO Change date to version
-ls $CONDA_BLD_PATH
+echo "bld"
+ls $CONDA_BLD_PATH/
+echo "linux"
+ls $CONDA_BLD_PATH/linux-64/
 echo "Converting conda package..."
 conda convert --platform all $CONDA_BLD_PATH/linux-64/$PKG_NAME-*.tar.bz2 --output-dir $CONDA_BLD_PATH/
-ls $CONDA_BLD_PATH
+ls $CONDA_BLD_PATH/linux-64/
 
 echo "Deploying to Anaconda.org..."
 anaconda -t $CONDA_UPLOAD_TOKEN upload $CONDA_BLD_PATH/**/$PKG_NAME-*.tar.bz2 --force
