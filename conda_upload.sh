10918ea600eb688831d03f8e54b49b4c28a029c2
acwikla <acwikla@novelaneuro.com>
2020-03-19 17:25:28 +0100

    travis fix

--- conda_upload.sh
+++ conda_upload.sh
@@ -8,7 +8,6 @@ conda config --set anaconda_upload no
 export ANACONDA_API_TOKEN=$CONDA_UPLOAD_TOKEN
 
 echo "Building conda package..."
-cd deploy_tools
 conda build . -q
 export CONDA_BUILD_PATH=/home/travis/miniconda/envs/test-environment/conda-bld
 
